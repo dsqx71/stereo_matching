74b19bbe9718e157e7af96f6e3b5889258c62af5
dsqx71 <dsqx71@gmail.com>
2016-03-09 17:27:17 +0800

    pred has problem

--- model.py
+++ model.py
@@ -1,17 +1,15 @@
 # -*- coding:utf-8 -*-  
 import mxnet as mx
-import mxnet as mx
 import numpy as np
 from collections import namedtuple
 from skimage import io
 from sklearn import utils
-from random import randint
-from random import shuffle
+from random import randint,shuffle,uniform,choice
+import cv2
 import matplotlib.pyplot as plt
 
-def get_network(network_type):
 
-
+def get_network(network_type,batch_size):
     relu = {}
     conv = {}
     weight = {}
@@ -20,101 +18,133 @@ def get_network(network_type):
     relu[1]  = mx.sym.Variable('right')
     relu[2]  = mx.sym.Variable('left_downsample')
     relu[3]  = mx.sym.Variable('right_downsample')
+    label    = mx.sym.Variable('label')
     for num_layer in range(1,5):
+        #blue parameter
         weight[0]   = mx.sym.Variable('l%d_blue' % num_layer)
+        bias[0]     = mx.sym.Variable('bias%d_blue' % num_layer)
+        #red  parameter 
         weight[1]   =  mx.sym.Variable('l%d_red' % num_layer)
-        bias[0]    = mx.sym.Variable('bias%d_blue' % num_layer)
-        bias[1]   = mx.sym.Variable('bias%d_red' % num_layer)
-        if num_layer<=2:
+        bias[1]      = mx.sym.Variable('bias%d_red' % num_layer)
+        
+        if num_layer <= 2:
             kernel = (3,3)
-            pad = (0,0)
             num_filter = 32
         else:
             kernel = (5,5)
-            pad = (0,0)
             num_filter = 200
+        
         for j in range(4):
-            conv[j]  = mx.sym.Convolution(data = relu[j] ,weight=weight[j/2],bias=bias[j/2],kernel=kernel,num_filter=num_filter,pad= pad)
-            relu[j] =  mx.sym.Activation(data=conv[j], act_type="relu")
-    
+            if network_type=='fully' and num_layer == 1:
+                conv[j]  = mx.sym.Convolution(data = relu[j] ,weight=weight[j/2],bias=bias[j/2],kernel=kernel,num_filter=num_filter,pad=(6,6))
+                relu[j]  = mx.sym.Activation( data = conv[j], act_type="relu")
+            else:
+                conv[j]  = mx.sym.Convolution(data = relu[j] ,weight=weight[j/2],bias=bias[j/2],kernel=kernel,num_filter=num_filter)
+                relu[j]  = mx.sym.Activation( data = conv[j], act_type="relu")
+        
     if network_type!='fully':
         flatten = {}        
         for j in range(4):
             flatten[j] = mx.sym.Flatten(data=relu[j])
-        s = mx.sym.Dotproduct(data1=flatten[0],data2=flatten[1])
-        net = mx.sym.Group([flatten[0],flatten[1],s])
+
+        s1 = mx.sym.Dotproduct(data1=flatten[0],data2=flatten[1])
+        s2 = mx.sym.Dotproduct(data1=flatten[2],data2=flatten[3])
+
+        s1 = mx.sym.Reshape(data=s1,target_shape = (batch_size,1,1,1))
+        s2 = mx.sym.Reshape(data=s2,target_shape = (batch_size,1,1,1))
+       
+        c1 = mx.sym.Convolution(data=s1,no_bias=True,kernel=(1,1),num_filter=1,name='w1')
+        c2 = mx.sym.Convolution(data=s2,no_bias=True,kernel=(1,1),num_filter=1,name='w2')
+        
+        c1 = mx.sym.Flatten(c1)
+        c2 = mx.sym.Flatten(c2)
+        net  = c1 + c2
+        net  = mx.sym.LinearRegressionOutput(data = net , label = label )
+        return net
     else:
-        net  = mx.sym.Group([relu[0],relu[1]])
-    return net
+        net  = mx.sym.Group([relu[0],relu[1],relu[2],relu[3]])
+        return net
 
 DataBatch = namedtuple('DataBatch', ['data', 'label', 'pad', 'index'])
 class dataiter(mx.io.DataIter):
 
-    def __init__(self,img_dir,batch_size,ctx,low,high,datatype='train'):
-        '''
-            img_dir: 图片位置 [(disparity_dir,left_dir,right_dir),.......]
-            low  high 决定负样本的相对于正样本的位置
-        '''
+    def __init__(self,img_dir,batch_size,ctx,datatype,high,low,rotate_range=3,contrast_range=1.3,brightness_range=0.7):
+    
         self.batch_size = batch_size
         self.reset()
         self.img_dir  = img_dir
         self.num_imgs = len(img_dir)
         self.datatype = datatype   
         self.ctx = ctx
-        self.low = low
+        self.rotate_range     = rotate_range
+        self.contrast_range   = contrast_range
+        self.brightness_range = brightness_range
         self.high = high
-     
-    def produce_patch(self,ith):
-        '''
-            self.l_ls : list of left patch
-        '''
-        dis  = np.round(io.imread(self.img_dir[ith][0])/256.0).astype(int)
-        left = io.imread(self.img_dir[ith][1]) - 128.0
-        left =  left.swapaxes(2,1).swapaxes(1,0)
-        right= io.imread(self.img_dir[ith][2]) - 128.0
-        right= right.swapaxes(2,1).swapaxes(1,0)
-        self.generate_patch_with_ground_truth(left,right,dis)  
-        utils.shuffle(self.l_ls,self.r_ls,self.ld_ls,self.rd_ls,self.labels)
+        self.low  = low
         
+    def produce_patch(self,ith):
+        dis  = np.round(io.imread(self.img_dir[ith][0])/256.0).astype(int)
+        left = io.imread(self.img_dir[ith][1])  
+        right= io.imread(self.img_dir[ith][2])  
+        
+        left =( left - left.reshape(-1,3).mean(axis=0) )/left.reshape(-1,3).std(axis=0)
+        right=( right- right.reshape(-1,3).mean(axis=0))/right.reshape(-1,3).std(axis=0)
+
+        self.generate_patch_with_ground_truth(left,right,dis)  
+        self.now_img = self.img_dir[ith][0]
+
+   
     def generate_patch_with_ground_truth(self,left,right,dis):
-        '''
-            generate patch from a img
-        '''
-        scale = 6
-        for y in xrange(scale,dis.shape[0]-scale):
-            for x in xrange(scale,dis.shape[1]-scale):
+        s1 = 6
+        s2 = 13
+        for y in xrange(s2,dis.shape[0]-s2):
+            for x in xrange(s2,dis.shape[1]-s2):
                 if dis[y,x]!=0:
+                    if np.random.random()>0.30:
+                        continue     
                     d = dis[y,x]
-                    if x-d>=scale :
-                        self.l_ls.append(left[:,y-scale:y+1+scale,x-scale:x+1+scale])
-                        self.r_ls.append(right[:,y-scale:y+1+scale,x-scale-d:x+1+scale-d])
-                        self.ld_ls.append(left[:,y-scale:y+1+scale:2,x-scale:x+1+scale:2])
-                        self.rd_ls.append(right[:,y-scale:y+1+scale:2,x-scale-d:x+1+scale-d:2])
+                    if x-d>=s2 :
+                        self.data[0].append( left[y-s1:y+1+s1,x-s1:x+1+s1,:])
+                        self.data[1].append(right[y-s1:y+1+s1,x-s1-d:x+1+s1-d,:])
+                        self.data[2].append(cv2.resize(left[y-s2:y+s2,x-s2:x+s2,:], (0,0), fx=0.5, fy=0.5))
+                        self.data[3].append(cv2.resize(right[y-s2:y+s2,x-s2-d:x+s2-d,:],(0,0), fx=0.5, fy=0.5))
                         self.labels.append(1)
                         while True:
-                            temp = [x - d + move for move in range(self.low,self.high+1)]
-                            temp.extend([x - d - move for move in range(self.low,self.high+1)])
+                            temp = [x - d + move for move in range(self.low,self.high+1) if x-d+move<dis.shape[1]-s2]
+                            temp.extend([x - d - move for move in range(self.low,self.high+1) if x-d-move>=s2]) 
                             xn = np.random.choice(temp)
-                            if xn<dis.shape[1]-scale and x-d != xn and xn>=scale:
+                            #xn = np.random.randint(s2,dis.shape[1]-s2-1)
+                            if xn<dis.shape[1]-s2 and x-d != xn and xn>=s2:
                                 break
-                        self.l_ls.append( left[:,y-scale:y+1+scale,    x-scale:x+1+scale])
-                        self.r_ls.append(right[:,y-scale:y+1+scale,xn-scale:xn+1+scale])
-                        self.ld_ls.append(left[:,y-scale:y+scale+1:2,x-scale:x+1+scale:2])
-                        self.rd_ls.append(right[:,y-scale:y+scale+1:2,xn-scale:xn+1+scale:2])
+                        self.data[0].append( left[y-s1:y+1+s1,    x-s1:x+1+s1,:])
+                        self.data[1].append(right[y-s1:y+1+s1,  xn-s1:xn+1+s1,:])
+                        self.data[2].append(cv2.resize(left[y-s2:y+s2,    x-s2:x+s2,:],(0,0),fx=0.5,fy=0.5))
+                        self.data[3].append(cv2.resize(right[y-s2:y+s2, xn-s2:xn+s2,:],(0,0),fx=0.5,fy=0.5))  
                         self.labels.append(0) 
-                        self.inventory +=2      
+                        self.inventory += 2  
+        #utils.shuffle([self.data[0],self.data[1],self.data[2],self.data[3],self.labels])
     
+      
+    def data_augment(self):
+
+        rotate = randint(-self.rotate_range,self.rotate_range)
+        rotation_matrix = cv2.getRotationMatrix2D((13/2, 13/2), rotate, 1)
+        beta  = uniform(0,self.brightness_range)
+        alpha = uniform(1,self.contrast_range)
+        flip_type = choice([-1,0,1])
+        for  i in range(4):
+            tmp = cv2.warpAffine(self.data[i][-1], rotation_matrix, (13, 13))
+            tmp = cv2.flip(tmp,flip_type)
+            tmp = cv2.multiply(tmp,np.array([alpha]))   
+            tmp = cv2.add(tmp,np.array([beta]))
+            self.data[i].append(tmp)
+           
     def reset(self):
-        '''
-          这几个list保存patch，inventory 表示dataiter剩余的patch数量
-        '''
+    
         self.index = 0
         self.img_idx = 0
         self.inventory = 0
-        self.l_ls = []
-        self.r_ls = []
-        self.ld_ls = []
-        self.rd_ls = []
+        self.data = [[],[],[],[]]
         self.labels = []
 
     def iter_next(self):
@@ -133,21 +163,21 @@ class dataiter(mx.io.DataIter):
             return True
 
     def getdata(self):
-
-        left = mx.nd.array(np.asarray(self.l_ls[:self.batch_size]),self.ctx)
-        right = mx.nd.array(np.asarray(self.r_ls[:self.batch_size]),self.ctx)
-        left_downsample = mx.nd.array(np.asarray(self.ld_ls[:self.batch_size]),self.ctx)
-        right_downsample = mx.nd.array(np.asarray(self.rd_ls[:self.batch_size]),self.ctx)
-        del self.l_ls[:self.batch_size]
-        del self.r_ls[:self.batch_size]
-        del self.ld_ls[:self.batch_size]
-        del self.rd_ls[:self.batch_size]
         
+        left  = mx.nd.array(np.asarray(self.data[0][:self.batch_size]).swapaxes(3,2).swapaxes(2,1),self.ctx)
+        right = mx.nd.array(np.asarray(self.data[1][:self.batch_size]).swapaxes(3,2).swapaxes(2,1),self.ctx)
+        left_downsample = mx.nd.array(np.asarray(self.data[2][:self.batch_size]).swapaxes(3,2).swapaxes(2,1),self.ctx)
+        right_downsample = mx.nd.array(np.asarray(self.data[3][:self.batch_size]).swapaxes(3,2).swapaxes(2,1),self.ctx)
+        del self.data[0][:self.batch_size]
+        del self.data[1][:self.batch_size]
+        del self.data[2][:self.batch_size]
+        del self.data[3][:self.batch_size]
+    
         return [left,right,left_downsample,right_downsample]
     
     def getlabel(self):
         if self.datatype !='test':
-            result =  mx.nd.array(self.labels[:self.batch_size])
+            result =  mx.nd.array(np.array(self.labels[:self.batch_size]))
             del self.labels[:self.batch_size]
             return result
         else :
@@ -158,32 +188,3 @@ class dataiter(mx.io.DataIter):
     
     def getpad(self):
         return 0
-
-
-'''       
-class Dot(mx.operator.NumpyOp):
-    def __init__(self):
-        super(Dot,self).__init__(True)
-    def list_arguments(self):
-        return ['x','y']
-    def list_outputs(self):
-        return ['output']
-    def infer_shape(self,in_shape):
-        data1_shape = in_shape[0]
-        data2_shape = in_shape[1]
-        output_shape = (in_shape[0][0],) 
-        return [data1_shape,data2_shape],[output_shape]
-    def forward(self,in_data,out_data):
-        x = in_data[0]
-        y = in_data[1]
-        output = out_data[0]
-        output[:] =  (x*y).sum(axis=1) 
-    def backward(self,out_grad,in_data,out_data,in_grad):
-        x = in_data[0]
-        y = in_data[1]
-        dx = in_grad[0]
-        dy = in_grad[1]
-        for i in xrange(out_grad[0].shape[0]):
-            dx[i] = out_grad[0][i] * y[i]
-            dy[i] = out_grad[0][i] * x[i]
-'''
--- train.py
+++ train.py
@@ -5,20 +5,14 @@ import matplotlib.pyplot as plt
 import mxnet as mx
 import numpy as np
 import logging
+import os
 import argparse
 from model import get_network,dataiter
-from util import get_kitty_data_dir,load_model,draw_patch
+from util import get_kitty_data_dir2012,get_kitty_data_dir2015,load_model,draw_patch,init,assign_grad_req
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                    datefmt ='%d %H:%M:%S')
-
-def init(key,weight):
-    if 'bias' in key:
-        weight[:] = 0
-    elif key not in data_sign:
-        weight[:] = mx.random.uniform(-0.007,0.007,weight.shape)    
-
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S')
 
 if __name__ == '__main__':
 
@@ -27,112 +21,92 @@ if __name__ == '__main__':
     parser.add_argument('--lr',action='store',dest='lr',type=float)
     parser.add_argument('--l',action='store',dest='low',type=int)
     parser.add_argument('--h',action='store',dest='high',type=int)
+
     cmd = parser.parse_args()
-
-    #cmd.con 不是指epoch，是指第几个轮，200个batch 为1轮 。 kitty dataset 跑一次epoch 需要3 小时
-
-    lr = cmd.lr
-    batch_size = 10000
+    batch_size = 5000
     s1 = (batch_size,3,13,13)
-    s2 = (batch_size,3,7,7)
+    s2 = (batch_size,3,27,27)
     ctx = mx.gpu(3) 
-    data_sign = ['left','right','left_downsample','right_downsample','label','LinearRegression_label','gt']
+    data_sign = ['left','right','left_downsample','right_downsample','label']
     
     if cmd.con == -1:
-        #重新训练
-        net = get_network('not fully')
-        executor = net.simple_bind(ctx=ctx,grad_req='add',left = s1,right= s1)
+        net = get_network('not fully',batch_size)
+        grad_req = assign_grad_req(net)
+        executor = net.simple_bind(ctx=ctx,grad_req=grad_req,left_downsample=s1,right_downsample=s1,left = s1,right= s1)
         keys  = net.list_arguments()
         grads = dict(zip(net.list_arguments(),executor.grad_arrays))
         args  = dict(zip(keys,executor.arg_arrays))
         auxs  = dict(zip(keys,executor.arg_arrays))
-        args['gt'] = mx.nd.zeros((batch_size,),ctx)
         logging.info("complete network architecture design")
-    
     else:
-        #继续之前的训练
-        net,executor = load_model('stereo',cmd.con,s1,'not fully',ctx)
+        net,executor = load_model('stereo',cmd.con,s1,s1,'not fully',ctx,batch_size)
         keys = net.list_arguments()
         grads = dict(zip(keys,executor.grad_arrays))
         args  = dict(zip(keys,executor.arg_arrays))
         auxs  = dict(zip(keys,executor.arg_arrays))
-        args['gt'] = mx.nd.zeros((batch_size,),ctx)
         logging.info("load the paramaters and net")
-
-
-    scale = 6
-    num_epoches = 10
-    train_iter =  dataiter(get_kitty_data_dir(0,175),batch_size,ctx,cmd.low,cmd.high,'train')
-    val_iter   =  dataiter(get_kitty_data_dir(175,180),batch_size,ctx,cmd.low,cmd.high,'valdiate')
+       
+    num_epoches = 10000
+    dirs = get_kitty_data_dir2015(0,180)
+    dirs.extend(get_kitty_data_dir2012(0,180))
+    train_iter =  dataiter(dirs,batch_size,ctx,'train',cmd.high,cmd.low,3,1.3,0.7)
     states     =  {}
-
-    #init args and optimizer
-    opt = mx.optimizer.ccSGD(learning_rate=lr,momentum=0.9,wd=0.000003,rescale_grad=(1.0/batch_size))
+    #init
+    opt = mx.optimizer.SGD(learning_rate=cmd.lr,momentum = 0.9,wd=0.00001,rescale_grad=(1.0/batch_size))
+   
     for index,key in enumerate(keys):
         if key not in data_sign:
             states[key] = opt.create_state(index,args[key])
             if cmd.con == -1 :
-                init(key,args[key])
-    
-    # train + validate 
-    for ith_epoche in range(num_epoches):
-        train_iter.reset()
-        val_iter.reset()
-        train_loss = 0.0
-        val_loss = 0.0
-        nbatch = 0
-        loss_of_30 = 0.0
-        #train
-        for dbatch in train_iter:
-            #load data
-            args['left'][:]  = dbatch.data[0]
-            args['right'][:] = dbatch.data[1]
-            args['gt'][:] = dbatch.label
-            nbatch += 1
-            
-            #forward 
-            executor.forward(is_train=True)
-            output = executor.outputs[2]
-            grad = 2*(output-args['gt'])
-            draw_patch(args,executor,train_iter.img_idx)
-            #calc loss
-            loss = (mx.nd.square(output-args['gt']).asnumpy()).mean()
-            train_loss += loss
-            loss_of_30 += loss
-            tmp = output.asnumpy()
-            acc = tmp[args['gt'].asnumpy()==1].mean()
-            err = tmp[args['gt'].asnumpy()==0].mean()
+                init(key,args[key],0.07)
 
-            logging.info("training: {}th pair img:{}th l2 loss:{} acc:{} err:{} >:{} lr:{}".format(nbatch,train_iter.img_idx,loss,acc,err,acc-err,opt.lr))
-            #30轮的平均loss
-            if nbatch % 30 == 0:
-                logging.info("mean loss of 30 batches: {} ".format(loss_of_30/30.0))
-                loss_of_30 = 0.0
-                #draw_patch(train_iter.img_idx)
+    # train + validate 
+    last_loss = 0.25
+    for ith_epoche in range(num_epoches):    
+
+        train_iter.reset()
+        train_loss = 0.0
+        nbatch = 0
+        loss_of_100 = 0.0
+
+        for dbatch in train_iter:
+           
+            args['left'][:]             = dbatch.data[0]
+            args['right'][:]            = dbatch.data[1]
+            args['left_downsample'][:]  = dbatch.data[2]
+            args['right_downsample'][:] = dbatch.data[3]
+            args['label'][:] = dbatch.label
+            nbatch += 1
         
-            #update args
-            executor.backward([mx.nd.zeros((batch_size,200),ctx=ctx),mx.nd.zeros((batch_size,200),ctx=ctx),grad])
+            executor.forward(is_train=True)
+            
+            #draw_patch(args,executor,train_iter.img_idx)
+            loss = np.power(executor.outputs[0].asnumpy() - args['label'].asnumpy().reshape(-1,1),2).mean()
+            train_loss  += loss
+            loss_of_100 += loss
+            tmp    = executor.outputs[0].asnumpy()
+            pos_ms = tmp[args['label'].asnumpy()==1].mean()
+            neg_ms = tmp[args['label'].asnumpy()==0].mean()
+            logging.info("training: {}th pair img:{}th l2 loss:{} pos_ms:{} neg_ms:{} >:{} lr:{}".format(nbatch,train_iter.img_idx,loss,pos_ms,neg_ms,pos_ms-neg_ms,opt.lr))
+       
+            if nbatch % 30 == 0:
+                loss_of_100 /=30.0
+                print train_iter.now_img 
+                logging.info("mean loss of 30 batches: {} ".format(loss_of_100))
+
+            executor.backward()
             for index,key in enumerate(keys):
-                if key not in data_sign:
+                if key not in data_sign:       
                     opt.update(index,args[key],grads[key],states[key])
                     grads[key][:] = np.zeros(grads[key].shape)
-            
-            if nbatch % 20==0:
-                cmd.con = (cmd.con + 1) % 2000
+
+            if nbatch % 50 == 0:
+                cmd.con = (cmd.con + 1) % 50
                 mx.model.save_checkpoint('stereo',cmd.con,net,args,auxs)
 
-        logging.info('training: ith_epoche :{} mean loss:{}'.format(ith_epoche,train_loss/float(nbatch)))
-        
-        #eval
-        nbatch = 0.0
-        for dbatch in val_iter:
-            args['left'][:]  = dbatch.data[0]
-            args['right'][:] = dbatch.data[1]
-            args['gt'][:] = dbatch.label
-            nbatch += 1
-            executor.forward(is_train=False)
-            loss = (mx.nd.square(executor.outputs[2]-args['gt']).asnumpy()).mean()
-            val_loss += loss
-            logging.info('eval:{}th pair img:{}th l2 loss:{}'.format(nbatch,val_iter.img_idx,loss))
-        logging.info('eval : ith_epoch :{} mean loss:{}'.format(ith_epoche,val_loss/float(nbatch)))
-    logging.info('Successful!')
+        train_loss/=nbatch
+        logging.info('training: ith_epoche :{} mean loss:{} last loss:{}'.format(ith_epoche,train_loss,last_loss))
+        if train_loss>last_loss+0.0001**ith_epoche:
+            opt.lr/= 0.1
+        last_loss = train_loss
+
